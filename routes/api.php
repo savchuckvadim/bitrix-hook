8cb4c191521bf9a0ecf5f85b7eeac07eac685137
savchuckvadim <savchuckvadim@gmail.com>
2023-12-15 10:08:06 +0100

    testing post

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,8 @@ Route::post('/register', [APIController::class, 'register']);
 Route::post('/forget-password', [APIController::class, 'forget_pass']);
 Route::post('/reset-password', [APIController::class, 'reset_pass']);
 
-Route::get('/test', function () {
+Route::post('/test', function () {
+    // Log::channel('single')->info('Request to /index', ['request' => $request->all()]);
     $response = Http::get('https://april-garant.bitrix24.ru/rest/1/1z69t5cso9s3vut7/crm.deal.add.json', [
         // Дополнительные параметры запроса, если необходимо
     ]);
