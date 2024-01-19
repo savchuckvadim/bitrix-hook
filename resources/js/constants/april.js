b19ff9c68cb5cc4ddb31098f93ee625444fc913f
savchuckvadim <savchuckvadim@gmail.com>
2024-01-19 08:55:56 +0100

    fxd hook api

--- resources/js/constants/april.js
+++ resources/js/constants/april.js
@@ -4,4 +4,4 @@ export const COMPANY_NAME = 'April'
 export const CREATED_BY = 'Vadim Savchuk'
 
 export const IS_APRIL_SIDEBAR = true
-export const IS_HOOK_SERVER_DEV = true 
+export const IS_HOOK_SERVER_DEV = false 
