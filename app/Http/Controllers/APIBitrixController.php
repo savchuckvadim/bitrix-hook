100ffba447b76cb4beef1aa0f5545b8ea2e2eaa2
savchuckvadim <savchuckvadim@gmail.com>
2024-01-19 12:09:45 +0100

    fxd while

--- app/Http/Controllers/APIBitrixController.php
+++ app/Http/Controllers/APIBitrixController.php
@@ -479,7 +479,7 @@ class APIBitrixController extends Controller
             $hook = 'https://' . $domain  . '/' . $webhookRestKey;
             $actionUrl = '/voximplant.statistic.get.json';
             $url = $hook . $actionUrl;
-            // do {
+            do {
             // Отправляем запрос на другой сервер
             $response = Http::get($url, [
                 "FILTER" => [
@@ -496,20 +496,20 @@ class APIBitrixController extends Controller
             // $data = $response->json();
 
             // Проверяем, что результат не равен 0 и не является пустым массивом
-            // if (!empty($response['result'])) {
-            //     // Делаем что-то с полученными данными
-            //     // Например, вы можете их обработать или сохранить
-            //     // и затем продолжить цикл или выйти из него, в зависимости от вашей логики
+            if (!empty($response['result'])) {
+                // Делаем что-то с полученными данными
+                // Например, вы можете их обработать или сохранить
+                // и затем продолжить цикл или выйти из него, в зависимости от вашей логики
 
-            //     foreach ($response['result'] as $call) {
-            //         array_push($resultCallings, $call);
-            //     }
-            // } else {
-            //     // Ждем некоторое время перед следующим запросом
-            //     sleep(5); // Например, ждем 5 секунд
-            // }
+                foreach ($response['result'] as $call) {
+                    array_push($resultCallings, $call);
+                }
+            } else {
+                // Ждем некоторое время перед следующим запросом
+                sleep(5); // Например, ждем 5 секунд
+            }
 
-            // } while (empty($response['result'])); // Продолжаем цикл, пока условие не выполнится
+            } while (empty($response['result'])); // Продолжаем цикл, пока условие не выполнится
 
 
             return APIOnlineController::getResponse(
