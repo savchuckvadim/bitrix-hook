e8da85e9f966e3813bfd4446b8763303912d4e6e
savchuckvadim <savchuckvadim@gmail.com>
2024-01-20 12:05:07 +0100

    wo task event handler

--- routes/api.php
+++ routes/api.php
@@ -381,7 +381,7 @@ Route::post('/update/smart/', function (Request $request) {
 
         //portal and keys
         $portal = PortalController::getPortal($domain);
-
+        Log::info('portal', ['portal' => $portal]);
         $portal = $portal['data'];
         Log::info('portalData', ['portal' => $portal]);
 
@@ -515,98 +515,58 @@ Route::get('/placement', function (Request $request) {
 
 
 
-Route::post('/bind', function (Request $request) {
-    //     http://portal.bitrix24.com/rest/placement.bind/?access_token=sode3flffcmv500fuagrprhllx3soi72
-    // 	&PLACEMENT=CRM_CONTACT_LIST_MENU
-    // 	&HANDLER=http%3A%2F%2Fwww.applicationhost.com%2Fplacement%2F
-    // 	&TITLE=Тестовое приложение
-    // HTTP/1.1 200 OK
-    // {
-    // 	"result": true
-    // }
-    // $portal = PortalController::getPortal($domain);
-    //
-    // $resultCallings = [];
-    try {
-        //CATEGORIES
-        // $webhookRestKey = $portal['data']['C_REST_WEB_HOOK_URL'];
-        // $hook = 'https://' . $domain  . '/' . $webhookRestKey;
-        // $actionUrl = '/voximplant.statistic.get.json';
-        // $url = $hook . $actionUrl;
-        Log::info('bind', ['bind' => $request->all()]);
-    } catch (\Throwable $th) {
-        return APIOnlineController::getError(
+// Route::post('/taskevent', function (Request $request) {
+//     //     http://portal.bitrix24.com/rest/placement.bind/?access_token=sode3flffcmv500fuagrprhllx3soi72
+//     // 	&PLACEMENT=CRM_CONTACT_LIST_MENU
+//     // 	&HANDLER=http%3A%2F%2Fwww.applicationhost.com%2Fplacement%2F
+//     // 	&TITLE=Тестовое приложение
+//     // HTTP/1.1 200 OK
+//     // {
+//     // 	"result": true
+//     // }
+//     $actionUrl = '/placement.bind.json';
+//     $domain = $request['auth']['domain'];
+//     $portal = PortalController::getPortal($domain);
+//     Log::info('portal', ['portal' => $portal]);
 
-            'error callings ' . $th->getMessage(),
-            [
-                // 'result' => $resultCallings,
+//     Log::info('taskevent', ['request' => $request->all()]);
+//     try {
 
-                'error callings ' . $th->getMessage(),
-            ]
-        );
-    }
-});
+//         $webhookRestKey = $portal['data']['C_REST_WEB_HOOK_URL'];
+//         $hook = 'https://' . $domain  . '/' . $webhookRestKey;
 
-
-Route::post('/taskevent', function (Request $request) {
-    //     http://portal.bitrix24.com/rest/placement.bind/?access_token=sode3flffcmv500fuagrprhllx3soi72
-    // 	&PLACEMENT=CRM_CONTACT_LIST_MENU
-    // 	&HANDLER=http%3A%2F%2Fwww.applicationhost.com%2Fplacement%2F
-    // 	&TITLE=Тестовое приложение
-    // HTTP/1.1 200 OK
-    // {
-    // 	"result": true
-    // }
-    $actionUrl = '/placement.bind.json';
-    $domain = $request['auth']['domain'];
-    $member_id = $request['auth']['member_id'];
-    $application_token = $request['auth']['application_token'];
-
-    $portal = PortalController::getPortal($domain);
+//         $url = $hook . $actionUrl;
+//         $data = [
+//             'PLACEMENT'=>'TASK_VIEW_SIDEBAR',
+//             'HANDLER'=>'https://april-server/test/placement.php',
+//             'LANG_ALL' => [
+//                 'en' => [
+//                     'TITLE' => 'Get Offer app',
+//                     'DESCRIPTION' => 'App Helps Garant employees prepare commercial documents and collect sales funnel statistics',
+//                     'GROUP_NAME' => 'Garant',
+//                 ],
+//                 'ru' => [
+//                     'TITLE' => 'КП Гарант',
+//                     'DESCRIPTION' => 'Приложение помогает сотрудникам Гарант составлять коммерческие документы и собирать статистику воронки продаж',
+//                     'GROUP_NAME' => 'группа',
+//                 ],
+//             ],
+//         ];
 
 
-    $hook = 'https://' . $domain . '/' . $member_id . '/' . $application_token . $actionUrl;
-    $handler = 'https://april-server/test/placement.php';
 
-    Log::info('taskevent', ['request' => $request->all()]);
-    try {
 
-        $webhookRestKey = $portal['data']['C_REST_WEB_HOOK_URL'];
-        // $hook = 'https://' . $domain  . '/' . $webhookRestKey;
+//         Log::info('taskevent', ['request' => $request->all()]);
+//     } catch (\Throwable $th) {
+//         Log::info('taskevent', ['request' => $request->all()]);
+//         return APIOnlineController::getError(
 
-        // $url = $hook . $actionUrl;
-        $data = [
-            'PLACEMENT' => 'CRM_ACTIVITY_LIST_MENU',
-            'HANDLER' => 'https://april-server/test/placement.php',
-            'LANG_ALL' => [
-                'en' => [
-                    'TITLE' => 'Get Offer app',
-                    'DESCRIPTION' => 'App helps Garant employees prepare commercial documents and collect sales funnel statistics',
-                    'GROUP_NAME' => 'Garant',
-                ],
-                'ru' => [
-                    'TITLE' => 'КП Гарант',
-                    'DESCRIPTION' => 'Приложение помогает сотрудникам Гарант составлять коммерческие документы и собирать статистику воронки продаж',
-                    'GROUP_NAME' => 'Гарант',
-                ],
-            ],
-        ];
-        // $headers = [
-        //     'Authorization' => 'Basic ' . base64_encode($member_id . ':' . $application_token),
-        // ];
-        $response = Http::get($hook, $data);
-        Log::info('taskevent', ['response' => $response]);
-        return $response;
-    } catch (\Throwable $th) {
-        Log::info('taskevent', ['request' => $request->all()]);
-        return APIOnlineController::getError(
+//             'error callings ' . $th->getMessage(),
+//             [
+//                 // 'result' => $resultCallings,
 
-            'error callings ' . $th->getMessage(),
-            [
-                // 'result' => $resultCallings,
-
-                'error callings ' . $th->getMessage(),
-            ]
-        );
-    }
-});
+//                 'error callings ' . $th->getMessage(),
+//             ]
+//         );
+//     }
+// });
