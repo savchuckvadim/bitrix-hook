23c8b4edab5d287c8747d34879b227a885c68cd0
savchuckvadim <savchuckvadim@gmail.com>
2024-05-24 17:11:22 +0700

    fxd admin

--- app/Http/Controllers/Front/Calling/ReportController.php
+++ app/Http/Controllers/Front/Calling/ReportController.php
@@ -11,16 +11,66 @@ class ReportController extends Controller
 {
     public static function eventReport(Request $request)
     {
+        try {
+            $data = [];
+            $isFullData = true;
 
-        return APIOnlineController::getSuccess(
-            [
-                'task' => [
-                    'message' => 'success'
-                ],
-                'rq' => $request
+            if (isset($request->currentTask)) {
+                $data['currentTask'] = $request->currentTask;
+            } else {
+                $isFullData = false;
+            }
+            if (isset($request->report)) {
+                $data['report'] = $request->report;
+            } else {
+                $isFullData = false;
+            }
+            if (isset($request->plan)) {
+                $data['plan'] = $request->plan;
+            } else {
+                $isFullData = false;
+            }
+            if (isset($request->placement)) {
+                $data['placement'] = $request->placement;
+            } else {
+                $isFullData = false;
+            }
+            if (isset($request->domain)) {
+                $data['domain'] = $request->domain;
+            } else {
+                $isFullData = false;
+            }
 
-            ]
+            if ($isFullData) {
+                return APIOnlineController::getSuccess(
+                    [
+                        'data' => $data
 
-        );
+                    ]
+
+                );
+            } else {
+                return APIOnlineController::getError(
+                    'is not full data',
+                    [
+                        'rq' => $request
+
+                    ]
+
+                );
+            }
+        } catch (\Throwable $th) {
+            return APIOnlineController::getError(
+                $th->getMessage(),
+                [
+                    'task' => [
+                        'message' => 'success'
+                    ],
+                    'rq' => $request
+
+                ]
+
+            );
+        }
     }
 }
