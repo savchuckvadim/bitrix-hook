062ada84cf235cf66918888844ef7c698b2b7180
savchuckvadim <savchuckvadim@gmail.com>
2024-06-17 22:34:04 +0700

    add logs

--- app/Services/HookFlow/BitrixListFlowService.php
+++ app/Services/HookFlow/BitrixListFlowService.php
@@ -47,13 +47,13 @@ class BitrixListFlowService
 
     ) {
         try {
-            // Log::channel('telegram')->info('failType list test', [
-            //     'eventType' => $eventType,
-            //     'noresultReason' => $noresultReason,
-            //     'failReason' => $failReason,
-            //     'failType' => $failType,
+            Log::channel('telegram')->info('failType list test', [
+                'eventType' => $eventType,
+                'noresultReason' => $noresultReason,
+                'failReason' => $failReason,
+                'failType' => $failType,
 
-            // ]);
+            ]);
 
             $nowDate = new DateTime();
 
@@ -375,7 +375,12 @@ class BitrixListFlowService
         } else if ($eventType === 'moneyAwait' || $eventType === 'money_await' || $eventType === 'money') {
             $result = 'call_in_money';
         }
+        Log::channel('telegram')->info('getEventType list test', [
+            'eventType' => $eventType,
+            'eventType result' => $result,
 
+
+        ]);
 
         return $result;
     }
