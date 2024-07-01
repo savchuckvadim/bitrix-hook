8e03106a10d7880722f0d9cbc66630540fcc6feb
savchuckvadim <savchuckvadim@gmail.com>
2024-07-01 12:29:50 +0300

    add session config

--- config/database.php
+++ config/database.php
@@ -145,6 +145,12 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
+        'session' => [
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'password' => env('REDIS_PASSWORD', null),
+        'port' => env('REDIS_PORT', 6379),
+        'database' => 1, // Убедитесь, что используете отличный от default индекс базы данных
+    ],
 
     ],
 
