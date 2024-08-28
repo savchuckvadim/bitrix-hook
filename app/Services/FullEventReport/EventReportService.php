e656bbb59e58667f671df58d0833a2e0de6706a3
savchuckvadim <savchuckvadim@gmail.com>
2024-08-28 13:02:45 +0300

    fxd results

--- app/Services/FullEventReport/EventReportService.php
+++ app/Services/FullEventReport/EventReportService.php
@@ -1916,8 +1916,8 @@ class EventReportService
         // ];
         $result = BitrixDealBatchFlowService::handleBatchResults($results);
 
-        Log::info('HOOK BATCH batchFlow', ['result' => $results]);
-        Log::channel('telegram')->info('HOOK BATCH batchFlow', ['result' => $results]);
+        Log::info('HOOK BATCH batchFlow', ['result' => $result]);
+        Log::channel('telegram')->info('HOOK BATCH batchFlow', ['result' => $result]);
 
 
         return  $result;
