85dd0c273041f44a3f570a9136187cfe9624f0f9
savchuckvadim <savchuckvadim@gmail.com>
2024-09-03 03:18:54 +0300

    next part

--- app/Http/Controllers/MigrateCRM/MColdFlowController.php
+++ app/Http/Controllers/MigrateCRM/MColdFlowController.php
@@ -63,7 +63,7 @@ class MColdFlowController extends Controller
             //         $clients = $googleData['clients'];
             //     }
             // }
-            $partsNumber = 6;
+            $partsNumber = 5;
 
 
             $time_start = microtime(true);
