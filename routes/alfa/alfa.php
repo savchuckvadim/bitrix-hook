01d6c0415087e7ce4723f99e60328af7fb87e1c2
savchuckvadim <savchuckvadim@gmail.com>
2024-09-20 13:48:56 +0300

    add alfa endpoint

---
+++ routes/alfa/alfa.php
@@ -0,0 +1,158 @@
+<?php
+
+use App\Http\Controllers\APIOnlineController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BitrixHookController;
+use App\Http\Controllers\PortalController;
+use App\Services\FullBatch\ColdBatchService;
+use App\Services\General\BitrixListService;
+use Illuminate\Support\Facades\Log;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+
+
+// Route::middleware(['rate.limit'])->group(function () {
+// новй холодный звонка из Откуда Угодно
+Route::post('alfa/contract-specification', function (Request $request) {
+   
+    $data = $request->all();
+    $companyId = $data['companyId'];
+    $smartId = $data['smartId'];
+    $domain = $data['auth']['domain'];
+    $listBitrixId = $data['listBitrixId'];
+    $hook = PortalController::getHook($domain);
+    $listFilter = [
+        // 'PROPERTY_' => $companyId,
+        'PROPERTY_192' => $smartId,
+
+    ];
+    $listItems  = BitrixListService::getList($hook, $listBitrixId, $listFilter);
+    //get smart -> smart list UF_CRM
+    Log::channel('telegram')->info('TST HOOK ALFA', [
+        'listItems' => $listItems
+    ]);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::post('/taskevent', function (Request $request) {
+//     //     http://portal.bitrix24.com/rest/placement.bind/?access_token=sode3flffcmv500fuagrprhllx3soi72
+//     // 	&PLACEMENT=CRM_CONTACT_LIST_MENU
+//     // 	&HANDLER=http%3A%2F%2Fwww.applicationhost.com%2Fplacement%2F
+//     // 	&TITLE=Тестовое приложение
+//     // HTTP/1.1 200 OK
+//     // {
+//     // 	"result": true
+//     // }
+//     $actionUrl = '/placement.bind.json';
+//     $domain = $request['auth']['domain'];
+//     $portal = PortalController::getPortal($domain);
+//     Log::info('portal', ['portal' => $portal]);
+
+//     Log::info('taskevent', ['request' => $request->all()]);
+//     try {
+
+//         $webhookRestKey = $portal['data']['C_REST_WEB_HOOK_URL'];
+//         $hook = 'https://' . $domain  . '/' . $webhookRestKey;
+
+//         $url = $hook . $actionUrl;
+//         $data = [
+//             'PLACEMENT'=>'TASK_VIEW_SIDEBAR',
+//             'HANDLER'=>'https://april-server/test/placement.php',
+//             'LANG_ALL' => [
+//                 'en' => [
+//                     'TITLE' => 'Get Offer app',
+//                     'DESCRIPTION' => 'App Helps Garant employees prepare commercial documents and collect sales funnel statistics',
+//                     'GROUP_NAME' => 'Garant',
+//                 ],
+//                 'ru' => [
+//                     'TITLE' => 'КП Гарант',
+//                     'DESCRIPTION' => 'Приложение помогает сотрудникам Гарант составлять коммерческие документы и собирать статистику воронки продаж',
+//                     'GROUP_NAME' => 'группа',
+//                 ],
+//             ],
+//         ];
+
+
+
+
+//         Log::info('taskevent', ['request' => $request->all()]);
+//     } catch (\Throwable $th) {
+//         Log::info('taskevent', ['request' => $request->all()]);
+//         return APIOnlineController::getError(
+
+//             'error callings ' . $th->getMessage(),
+//             [
+//                 // 'result' => $resultCallings,
+
+//                 'error callings ' . $th->getMessage(),
+//             ]
+//         );
+//     }
+// });
