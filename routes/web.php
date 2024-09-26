199a894e5b22c69699478e0787dd9f81ab3e795e
savchuckvadim <savchuckvadim@gmail.com>
2024-09-26 07:38:21 +0300

    add test

--- routes/web.php
+++ routes/web.php
@@ -23,19 +23,21 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('/test/', function ($pass, $domain, $token) {
+Route::get('/test/3479', function () {
 
     $domain = 'april-dev.bitrix24.ru';
     $portal = PortalController::getPortal($domain);
-    if(!empty($portal)){
+    if(!empty($portal) && !empty($portal['data'])){
         $result = $portal['data']['id'];
 
+    }else{
+        $result = $portal;
+
     }
-    // dd([
-    //     'pass' => $pass,
-    //     'domain' => $domain,
-    //     'token' => $token,
-    // ]);
+    dd([
+        'result' => $result,
+        
+    ]);
 
     return 'yo';
 });
