369bcac925f70ed95df0f0794fba8d721705db3d
savchuckvadim <savchuckvadim@gmail.com>
2024-11-04 16:44:17 +0200

    fxd department

--- app/Http/Controllers/Front/EventCalling/ReportController.php
+++ app/Http/Controllers/Front/EventCalling/ReportController.php
@@ -2497,11 +2497,9 @@ class ReportController extends Controller
                 }
             }
 
-            // if (empty($departmentResult)) {                               // если в сессии нет department
+            if (empty($departmentResult)) {                               // если в сессии нет department
                 $departamentService = new BitrixDepartamentService($hook);
-                $result['portal'] = $portal;
                 $department =  $departamentService->getDepartamentIdByPortal($portal);
-                $result['getDepartamentIdByPortal'] = $department;
                 $allUsers = [];
                 if (!empty($department)) {
 
@@ -2564,7 +2562,7 @@ class ReportController extends Controller
                         );
                     }
                 }
-            // }
+            }
 
 
             return APIOnlineController::getSuccess(
