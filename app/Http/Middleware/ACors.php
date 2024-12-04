a2c704e498615155772a18d7483be05c580efd09
savchuckvadim <savchuckvadim@gmail.com>
2024-12-04 19:49:43 +0200

    wt mw

--- app/Http/Middleware/ACors.php
+++ app/Http/Middleware/ACors.php
@@ -14,13 +14,27 @@ class ACors
      */
     public function handle($request, Closure $next)
     {
+        // Обработка предварительных запросов (OPTIONS)
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json([], 200, [
+                'CORS-Middleware-Called' => 'true',
+                'Access-Control-Allow-Origin' => 'https://front.april-app.ru',
+                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials' => 'true',
+            ]);
+        }
+    
+        // Обработка обычных запросов
         $response = $next($request);
+    
         $response->headers->set('CORS-Middleware-Called', 'true');
         $response->headers->set('Access-Control-Allow-Origin', 'https://front.april-app.ru');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
-
+    
         return $response;
     }
+    
 }
