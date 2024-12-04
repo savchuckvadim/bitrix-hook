0b64602a80136bc3ca42365f44215796fbc871e4
savchuckvadim <savchuckvadim@gmail.com>
2024-12-04 19:39:10 +0200

    fxd middle

--- app/Http/Middleware/ACors.php
+++ app/Http/Middleware/ACors.php
@@ -16,7 +16,7 @@ class ACors
     {
         $response = $next($request);
         $response->headers->set('CORS-Middleware-Called', 'true');
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://front.april-app.ru');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
