63f92350447f4afa07079432a277a9c359e04fce
savchuckvadim <savchuckvadim@gmail.com>
2024-12-07 16:27:00 +0200

    fxd inner conf

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,14 +20,14 @@ class Kernel extends HttpKernel
 
         // \App\Http\Middleware\TrustHosts::class,
 
-        \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
+        // \App\Http\Middleware\TrustProxies::class,
+        // \Illuminate\Http\Middleware\HandleCors::class,
 
 
-        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        // \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        // \App\Http\Middleware\TrimStrings::class,
+        // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
@@ -47,8 +47,6 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-
-
 
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -84,7 +82,7 @@ class Kernel extends HttpKernel
             'path' => $request->path(),
             'headers' => $request->headers->all(),
         ];
-        print_r($data);
+        // print_r($data);
 
        
         return parent::handle($request);
