f33b15889c89651e8766743b64bf834fb5023f5c
savchuckvadim <savchuckvadim@gmail.com>
2025-01-23 20:52:35 +0200

    add logs

--- app/Http/Controllers/Front/EventCalling/Lead/FullEventFlowLeadController.php
+++ app/Http/Controllers/Front/EventCalling/Lead/FullEventFlowLeadController.php
@@ -18,81 +18,99 @@ class FullEventFlowLeadController extends Controller
     {
 
         $data = $request->all();
+        try {
+            $leadId = $data['leadId'];
+            $deadline = $data['deadline'];
+            $assigned = $data['assigned'];
+            $name = $data['name'];
+            $domain  = $data['auth']['domain'];
+            $hook = PortalController::getHook($domain);
+            $responsibleId = null;
+            $createdId = 1;
+            $companyId = null;
 
-        $leadId = $data['leadId'];
-        $deadline = $data['deadline'];
-        $assigned = $data['assigned'];
-        $name = $data['name'];
-        $domain  = $data['auth']['domain'];
-        $hook = PortalController::getHook($domain);
-        $responsibleId = null;
-        $createdId = 1;
-        $companyId = null;
+            if (isset($assigned)) {
 
-        if (isset($assigned)) {
+                $partsResponsible = explode("_", $assigned);
 
-            $partsResponsible = explode("_", $assigned);
+                $responsibleId = $partsResponsible[1];
+            }
 
-            $responsibleId = $partsResponsible[1];
+
+            APIOnlineController::sendLog('FullEventFlowLeadController', [
+
+                'leadId' => $leadId,
+                'domain' => $domain,
+                'deadline' => $deadline,
+                'assigned' => $assigned,
+            ]);
+            $lead = BitrixGeneralService::getEntityByID($hook, 'lead', $leadId);
+
+            $fields = [];
+            $fields['LEAD_ID'] = $leadId;
+
+            //UF_CRM_LEAD_QUEST_URL ссылка на отчет
+            if (!empty($lead['COMPANY_ID'])) {
+                $companyId = $lead['COMPANY_ID'];
+                BitrixGeneralService::updateEntity($hook, 'company', $companyId,   $fields);
+            } else {
+                $fields['TITLE'] = $lead['TITLE'];
+                $companyId = BitrixGeneralService::setEntity($hook, 'company', $fields);
+            }
+
+
+            // $fields['ASSIGNED_BY_ID'] = $lead['ASSIGNED_BY_ID'];
+            // $fields['PHONE'] = $lead['PHONE'];
+            // $fields['EMAIL'] = $lead['EMAIL'];
+
+
+            $leadUpdate = BitrixGeneralService::updateEntity($hook, 'lead', $leadId,   [
+                'COMPANY_ID' => $companyId,
+                //  'STATUS_ID' => 'PROCESSED'
+                // 'STATUS_ID' => 'CONVERTED'
+            ]);
+
+            APIOnlineController::sendLog('FullEventFlowLeadController', [
+
+                'leadUpdate' => $leadUpdate,
+
+            ]);
+
+
+            $prejob_data = [
+                'domain' => $domain,
+                'entityType' => 'company',
+                'entityId' => $companyId,
+                'responsible' => $responsibleId,
+                'created' => $createdId,
+                'deadline' => $deadline,
+                'name' => $name,
+                'lead' => $lead,
+                'isTmc' => false
+
+            ];
+            APIOnlineController::sendLog('FullEventFlowLeadController', [
+
+                'prejob data' => $data,
+
+            ]);
+            dispatch(
+                new ColdBatchJob($prejob_data)
+            )->onQueue('low-priority');
+        } catch (\Throwable $th) {
+            $errorMessages =  [
+                'message'   => $th->getMessage(),
+                'file'      => $th->getFile(),
+                'line'      => $th->getLine(),
+                'trace'     => $th->getTraceAsString(),
+            ];
+            APIOnlineController::sendLog('FullEventFlowLeadController', [
+
+                'prejob data' => $data,
+                'companyId' => $companyId,
+                'error' =>   $errorMessages
+
+            ]);
         }
-
-
-        APIOnlineController::sendLog('FullEventFlowLeadController', [
-
-            'leadId' => $leadId,
-            'domain' => $domain,
-            'deadline' => $deadline,
-            'assigned' => $assigned,
-        ]);
-        $lead = BitrixGeneralService::getEntityByID($hook, 'lead', $leadId);
-
-        $fields = [];
-        $fields['LEAD_ID'] = $leadId;
-   
-        //UF_CRM_LEAD_QUEST_URL ссылка на отчет
-        if (!empty($lead['COMPANY_ID'])) {
-            $companyId = $lead['COMPANY_ID'];
-            BitrixGeneralService::updateEntity($hook, 'company', $companyId,   $fields);
-        }else{
-            $fields['TITLE'] = $lead['TITLE'];
-            $companyId = BitrixGeneralService::setEntity($hook, 'company', $fields);
-
-        }
-
-     
-        // $fields['ASSIGNED_BY_ID'] = $lead['ASSIGNED_BY_ID'];
-        // $fields['PHONE'] = $lead['PHONE'];
-        // $fields['EMAIL'] = $lead['EMAIL'];
-
-
-        $leadUpdate = BitrixGeneralService::updateEntity($hook, 'lead', $leadId,   [
-            'COMPANY_ID' => $companyId,
-            //  'STATUS_ID' => 'PROCESSED'
-            // 'STATUS_ID' => 'CONVERTED'
-        ]);
-
-        APIOnlineController::sendLog('FullEventFlowLeadController', [
-
-            'leadUpdate' => $leadUpdate,
-
-        ]);
-
-
-        $data = [
-            'domain' => $domain,
-            'entityType' => 'company',
-            'entityId' => $companyId,
-            'responsible' => $responsibleId,
-            'created' => $createdId,
-            'deadline' => $deadline,
-            'name' => $name,
-            'lead' => $lead,
-            'isTmc' => false
-
-        ];
-
-        dispatch(
-            new ColdBatchJob($data)
-        );
     }
 }
