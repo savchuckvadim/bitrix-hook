e667cbf5a046ff3222dda713c8b831b79128ab8b
savchuckvadim <savchuckvadim@gmail.com>
2025-01-23 16:41:34 +0200

    fx route

--- routes/full/full_event.php
+++ routes/full/full_event.php
@@ -19,9 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/pres/count', function (Request $request) {
     return ReportController::getPresCounts($request);
 });
+require __DIR__ . '/full_lead.php';
+
 
 Route::prefix('full')->group(function () {
-    require __DIR__ . '/full_lead.php';
 
 
     Route::post('', [ReportController::class, 'eventReport']);
--- routes/full/full_lead.php
+++ routes/full/full_lead.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Front\EventCalling\Lead\FullEventFlowLeadController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/lead')->group(function () {
+Route::prefix('flow-lead')->group(function () {
 
     Route::post('/cold', [FullEventFlowLeadController::class, 'cold']);
 
