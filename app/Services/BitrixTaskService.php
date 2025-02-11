34ff528c50e11ea35b7530006a3169aec5cb2f6d
savchuckvadim <savchuckvadim@gmail.com>
2025-02-11 14:06:30 +0300

    tst priority

--- app/Services/BitrixTaskService.php
+++ app/Services/BitrixTaskService.php
@@ -395,7 +395,7 @@ class BitrixTaskService
                     'DEADLINE' => $moscowTime, //- крайний срок;
                     'UF_CRM_TASK' => $crmItems,
                     'ALLOW_CHANGE_DEADLINE' => 'Y',
-                    'PRIORITY' => 2
+                    'PRIORITY' => 1
                     // 'DESCRIPTION' => $description
                 ]
             ];
